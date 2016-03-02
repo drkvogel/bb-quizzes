d3e713b1d506dcdfc62c03b8248ea553a908b95b
Chris @ q108-vlubuntu <cbird@q108-vlubuntu>
2016-03-02 16:18:44 +0000

    more

--- matrix/app/scripts/main.js
+++ matrix/app/scripts/main.js
@@ -4,6 +4,7 @@ $("#content-container").on('click', 'a', function(e) { // delegate events
 	//$(this).parent().html('<a href="#">A New link</a>');
 	// $(this).load("snip.html#snip1");  // try to load only div from file - doesn't work
 	//$(this).load("snip.html"); 
+	switch ($(this)
 	$(this).load("snip.html"); 
 	e.preventDefault();
 });
@@ -28,3 +29,22 @@ $("#content-container").on('click', 'a', function(e) { // delegate events
 		// or start Chrome with --allow-file-access-from-files
 		// http://stackoverflow.com/questions/4819060/allow-google-chrome-to-use-xmlhttprequest-to-load-a-url-from-a-local-file
 });*/
+
+
+// http://www.attackofdesign.com/using-jquery-to-include-files/
+
+// <div class="js-include" title="nav.html"></div>
+
+// “js-include” is simply a class name that we’ll assign to every element that should trigger the include. And “nav.html” is the relative URL of the file that we’re including.
+// Now, let’s take a look at the jQuery code:
+
+$(".js-include").each(function(){
+    var inc=$(this);
+    $.get(inc.attr("title"), function(data){
+        inc.replaceWith(data);
+    });
+});
+// The first line just tells the script to execute the function once of each element that has the class “js-include”.
+// We then store the current element inside a variable
+// We use jQuery’s get function with the following arguments: the div’s title as the URL of the file to load, and a callback function
+// The callback function replaces the current DIV with the content that we just loaded.
