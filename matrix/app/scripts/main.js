72ebe8ee3deb70a43b2a057724f99fd96dee8785
Chris Bird <chrisjbird@gmail.com>
2016-03-07 13:45:48 +0000

    get rid of lint errors

--- matrix/app/scripts/main.js
+++ matrix/app/scripts/main.js
@@ -1,32 +1,45 @@
-"use strict";
+'use strict';
 
 console.log('Starting Matrix Puzzle');
 //console.log('NUM_PAGES: '+NUM_PAGES);
+
 
 // data.js
 
 //var pages   = ['home', 'intro1', 'intro2', 'thanks', 'abandon', 'quiz2x2', 'quiz3x3'];
 
 
-
+var numPages = 19; // temp
 var pages = ['home', 'intro1', 'intro2', 'thanks', 'abandon', 'quiz2x2', 'quiz3x3', 'matrix_ex1', 'matrix_ex2', 'matrix_ex3', 'matrix_ex4', 'matrix_ex5', 'matrix_ex6', 'matrix_ex7'];
 var current = 0; // 'home';
 
 // function preload() {
-// 	//images[25] = new Image();
+//     //images[25] = new Image();
 //     //ges[25].src = 'Snap/snap_images/Rear.GIF';
 // }
 
 function showPage(page) {
-    var pageId='#'+page;
-    console.log('showPage('+page+'), pageId: '+pageId);
-	$('#content-container').html($(pageId));
+    var pageId = '#' + page;
+    console.log('showPage(' + page + '), pageId: ' + pageId);
+    $('#content-container').html($(pageId));
+}
+
+function prevPage() {
+    if (current > 0) { current--; }
+    console.log('prevPage(): current: ' + current + ', pages[current]: ' + pages[current]);
+    showPage(pages[current]);
+}
+
+function nextPage() {
+    if (current < numPages) { current++; }
+    console.log('nextPage(): current: ' + current + ', pages[current]: ' + pages[current]);
+    showPage(pages[current]);
 }
 
 $('#content-container').on('click', 'a, button', function(e) { // delegate events
     e.preventDefault();
     var pageId = $('.page').attr('id');
-    console.log('pageId: '+pageId); // now gets id from loaded page
+    console.log('pageId: ' + pageId); // now gets id from loaded page
     var clickedEl = $(this);
     nextPage();
     switch (clickedEl.attr('id')) {
@@ -35,48 +48,42 @@ $('#content-container').on('click', 'a, button', function(e) { // delegate event
         case 'yes':
         case 'no':
             //console.log('elid: '+clickedEl.attr('id')+', html: ''+clickedEl.html()+''');
-            console.log('elid: '+clickedEl.attr('id'));
-            break;
+        console.log('elid: ' + clickedEl.attr('id'));
+        break;
         default:
-            console.log('got unexpected element id: '+clickedEl.attr('id')); //+', html: ''+clickedEl.html()+''');
+            console.log('got unexpected element id: ' + clickedEl.attr('id')); //+', html: ''+clickedEl.html()+''');
             //alert('got id: ');
     }
 });
 
-function prevPage() {
-    if (current > 0) current--;
-    console.log('prevPage(): current: '+current+', pages[current]: '+pages[current]);
-    showPage(pages[current]);
-}
 
-function nextPage() {
-    if (current < NUM_PAGES) current++;
-    console.log('nextPage(): current: '+current+', pages[current]: '+pages[current]);
-    showPage(pages[current]);
-}
 
 $('#buttons').on('click', 'a, button', function(e) { // delegate events
     e.preventDefault();
     var pageId = $('.page').attr('id'); //console.log('pageId: '+pageId); // now gets id from loaded page
     var clickedEl = $(this);
-    console.log('pageId: '+pageId+': elid: '+clickedEl.attr('id')); //console.log('elid: '+clickedEl.attr('id')+', html: ''+clickedEl.html()+''');
+    console.log('pageId: ' + pageId + ': elid: ' + clickedEl.attr('id')); //console.log('elid: '+clickedEl.attr('id')+', html: ''+clickedEl.html()+''');
     switch (clickedEl.attr('id')) {
-        case 'prev': prevPage(); break;
-        case 'next': nextPage(); break;
-        case 'yes':  console.log('yes'); break;
-        case 'no':   console.log('no');  break;
+        case 'prev':
+            prevPage(); break;
+        case 'next':
+            nextPage(); break;
+        case 'yes':
+            console.log('yes'); break;
+        case 'no':
+            console.log('no'); break;
         default:
-            console.log('got unexpected element id: '+clickedEl.attr('id')); //+', html: ''+clickedEl.html()+''');
+            console.log('got unexpected element id: ' + clickedEl.attr('id')); //+', html: ''+clickedEl.html()+''');
     }
 });
 
 $(document).ready(function() {
     console.log(' Fri Mar  4 12:11:23 2016 Ready');
     showPage('home');
-    asfadf
+    //asfadf
     //$('#content-container').html($('#thanks'));
 });
-  
+
     //var page = $.get('./pages/pages.html', function(data) {$('#content-container').html($(data).filter('#intro2')); });
 
 
@@ -97,25 +104,25 @@ $(document).ready(function() {
 
 
 
-	//$(this).parent().html('<a href='#'>A New link</a>');
-	// $(this).load('snip.html#snip1');  // try to load only div from file - doesn't work
-	//$(this).load('snip.html');
-	//switch ($('.page').
+    //$(this).parent().html('<a href='#'>A New link</a>');
+    // $(this).load('snip.html#snip1');  // try to load only div from file - doesn't work
+    //$(this).load('snip.html');
+    //switch ($('.page').
 
 
 /*$(document).ready(function() {
-	console.log('document ready!');
+    console.log('document ready!');
 
     //$('#list a').on('click', function(event) { // Attach a directly bound event handler
         // attached to each '#list a' at doc ready, but not to extra ones added afterwards
-	$('#list').on('click', 'a', function(event) { // event delegated to '#list' catches those appended 'a's
-	    event.preventDefault();
-	    console.log('text(): '+$(this).text());
+    $('#list').on('click', 'a', function(event) { // event delegated to '#list' catches those appended 'a's
+        event.preventDefault();
+        console.log('text(): '+$(this).text());
         console.log('attr(): '+$(this).attr('href')); // http://api.jquery.com/attr/
-	    //var elem = $(this); //console.log('getAttribute(): '+elem.getAttribute('href'));
-	});
+        //var elem = $(this); //console.log('getAttribute(): '+elem.getAttribute('href'));
+    });
 
-	$('#list').append('<li><a href='#'>another one!</a></li>');
+    $('#list').append('<li><a href='#'>another one!</a></li>');
     $('#content').load('snip.html'); //$('body').append($.load('snip.html'));
         // can't be done from file on Chrome due to
         // serve over HTTP e.g. with python -m SimpleHTTPServer
