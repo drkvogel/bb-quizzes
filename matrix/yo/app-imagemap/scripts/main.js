aa53b74bf5bf3d0794822407cf7697b51849bb96
Chris @ q108-vlubuntu <cbird@q108-vlubuntu>
2016-06-30 11:35:02 +0100

    matrix: .quizContainer to allow text to expand whilst resizing grids with normalizeWidths()

--- matrix/yo/app-imagemap/scripts/main.js
+++ matrix/yo/app-imagemap/scripts/main.js
@@ -222,8 +222,10 @@
     function normalizeWidth() {
         var margin = ($(window).width() - $(window).height()) / 2; 
         console.log('$(window).width(): ' + $(window).width() + ', $(window).height()' + $(window).height() + ', margins: ' + margin); 
-        $('.container').css('margin-left', margin); // set the margins to (screen width - height) / 2
-        $('.container').css('margin-right', margin); //$('.container').attr('width', $(window).height()); // doesn't work but setting margin-left and margin-right makes it shrink
+        $('.quizContainer').css('margin-left', margin); // set the margins to (screen width - height) / 2
+        $('.quizContainer').css('margin-right', margin); //$('.container').attr('width', $(window).height()); // doesn't work but setting margin-left and margin-right makes it shrink
+        // $('.container').css('margin-left', margin); // set the margins to (screen width - height) / 2
+        // $('.container').css('margin-right', margin); //$('.container').attr('width', $(window).height()); // doesn't work but setting margin-left and margin-right makes it shrink
     }
 
     function showPage(page) { // prevPage() and nextPage() should handle hiding current
