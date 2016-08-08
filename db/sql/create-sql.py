f06963aa20dcd103ae54eac5aa2c4946fe3ab264
Chris @ q108-vlubuntu <cbird@q108-vlubuntu>
2016-08-08 16:08:18 +0100

    more

--- db/sql/create-sql.py
+++ db/sql/create-sql.py
@@ -1,11 +1,15 @@
 
 
-# 
+last = 18
 
-for i in range(1, 19):
+for i in range(1, last + 1):
     print "    duration" + str(i) + "    DATE          NOT NULL,"
     print "    puzzle" + str(i) + "      INTEGER       NOT NULL,"
     print "    elapsed" + str(i) + "     DATE          NOT NULL,"
     print "    answer" + str(i) + "      INTEGER       NOT NULL,"
-    print "    correct" + str(i) + "     INTEGER       NOT NULL,"
+    if i == last:
+        print "    correct" + str(i) + "     INTEGER       NOT NULL" # take off final comma
+    else:
+        print "    correct" + str(i) + "     INTEGER       NOT NULL,"    
     print
+
