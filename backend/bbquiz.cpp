f0f6c60ef8fb84e066481ffd3b7efcbfaf8b9545
drkvogel <drkvogel@yahoo.co.uk>
2016-09-27 16:58:13 +0100

    backend: accept quiz_id 1111 for matrix

--- backend/bbquiz.cpp
+++ backend/bbquiz.cpp
@@ -128,7 +128,7 @@ int main(int argc, char **argv) {
             } else {
                 printf("<p>insert did not succeed...</p>\n");
             }
-        } else if (paramIs("action", "insert") && paramIs("quiz", "1111")) {
+        } else if (paramIs("action", "insert") && paramIs("quiz_id", "1111")) {
             Matrix::Record rec = Matrix::getPayload(x);
             if (Matrix::insertRecord(&rec)) {
                 printf("<p>insert succeeded</p>\n");
